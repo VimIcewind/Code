b96c9c883b7624ca3fa7fa9924555876616a8c75
VimIcewind <VimIcewind@gmail.com>
2017-03-16 16:56:29 +0800

    this

---
+++ cpp/Runoob/this.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+class Box
+{
+public:
+	Box(double l = 2.0, double b = 2.0, double h = 2.0)
+	{
+		cout << "Constructor called." << endl;
+		length = l;
+		breadth = b;
+		height = h;
+	}
+
+	double Volume()
+	{
+		return length * breadth * height;
+	}
+
+	int compare(Box box)
+	{
+		return this->Volume() > box.Volume();
+	}
+
+private:
+	double length;
+	double breadth;
+	double height;
+};
+
+int main(void)
+{
+	Box Box1(3.3, 1.2, 1.5);
+	Box Box2(8.5, 6.0, 2.0);
+
+	if (Box1.compare(Box2))
+	{
+		cout << "Box2 is smaller than Box1" << endl;
+	}
+	else
+	{
+		
+		cout << "Box2 is equal to or larger than Box1" << endl;
+	}
+}
