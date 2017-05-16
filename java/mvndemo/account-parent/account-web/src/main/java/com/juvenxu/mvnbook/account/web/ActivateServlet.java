f2ce62ed759d8e0cef39087cdd8104be1ccc810c
VimIcewind <VimIcewind@gmail.com>
2017-05-16 17:14:04 +0800

    Just format code.

--- java/mvndemo/account-parent/account-web/src/main/java/com/juvenxu/mvnbook/account/web/ActivateServlet.java
+++ java/mvndemo/account-parent/account-web/src/main/java/com/juvenxu/mvnbook/account/web/ActivateServlet.java
@@ -13,44 +13,33 @@ import org.springframework.web.context.support.WebApplicationContextUtils;
 import com.juvenxu.mvnbook.account.service.AccountService;
 import com.juvenxu.mvnbook.account.service.AccountServiceException;
 
-public class ActivateServlet
-    extends HttpServlet
-{
+public class ActivateServlet extends HttpServlet {
     private static final long serialVersionUID = 3668445055149826106L;
 
     private ApplicationContext context;
 
     @Override
-    public void init()
-        throws ServletException
-    {
+    public void init() throws ServletException {
         super.init();
-        context = WebApplicationContextUtils.getWebApplicationContext( getServletContext() );
+        context = WebApplicationContextUtils.getWebApplicationContext(getServletContext());
     }
 
     @Override
-    protected void doGet( HttpServletRequest req, HttpServletResponse resp )
-        throws ServletException,
-            IOException
-    {
-        String key = req.getParameter( "key" );
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String key = req.getParameter("key");
 
-        if ( key == null || key.length() == 0 )
-        {
-            resp.sendError( 400, "No activation key provided." );
+        if (key == null || key.length() == 0) {
+            resp.sendError(400, "No activation key provided.");
             return;
         }
 
-        AccountService service = (AccountService) context.getBean( "accountService" );
+        AccountService service = (AccountService) context.getBean("accountService");
 
-        try
-        {
-            service.activate( key );
-            resp.getWriter().write( "Account is activated, now you can login." );
-        }
-        catch ( AccountServiceException e )
-        {
-            resp.sendError( 400, "Unable to activate account" );
+        try {
+            service.activate(key);
+            resp.getWriter().write("Account is activated, now you can login.");
+        } catch (AccountServiceException e) {
+            resp.sendError(400, "Unable to activate account");
             return;
         }
     }
--- java/mvndemo/account-parent/account-web/src/main/java/com/juvenxu/mvnbook/account/web/LoginServlet.java
+++ java/mvndemo/account-parent/account-web/src/main/java/com/juvenxu/mvnbook/account/web/LoginServlet.java
@@ -13,8 +13,7 @@ import org.springframework.web.context.support.WebApplicationContextUtils;
 import com.juvenxu.mvnbook.account.service.AccountService;
 import com.juvenxu.mvnbook.account.service.AccountServiceException;
 
-public class LoginServlet extends HttpServlet
-{
+public class LoginServlet extends HttpServlet {
     private static final long serialVersionUID = 929160785365121624L;
 
     private ApplicationContext context;
@@ -22,26 +21,26 @@ public class LoginServlet extends HttpServlet
     @Override
     public void init() throws ServletException {
         super.init();
-        context = WebApplicationContextUtils.getWebApplicationContext( getServletContext() );
+        context = WebApplicationContextUtils.getWebApplicationContext(getServletContext());
     }
 
     @Override
-    protected void doPost( HttpServletRequest req, HttpServletResponse resp ) throws ServletException, IOException {
-        String id = req.getParameter( "id" );
-        String password = req.getParameter( "password" );
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+        String password = req.getParameter("password");
 
-        if ( id == null || id.length() == 0 || password == null || password.length() == 0 ) {
-            resp.sendError( 400, "incomplete parameter" );
+        if (id == null || id.length() == 0 || password == null || password.length() == 0) {
+            resp.sendError(400, "incomplete parameter");
             return;
         }
 
-        AccountService service = (AccountService) context.getBean( "accountService" );
+        AccountService service = (AccountService) context.getBean("accountService");
 
         try {
-            service.login( id, password );
-            resp.getWriter().print( "Login Successful!" );
-        } catch ( AccountServiceException e ) {
-            resp.sendError( 400, e.getMessage() );
+            service.login(id, password);
+            resp.getWriter().print("Login Successful!");
+        } catch (AccountServiceException e) {
+            resp.sendError(400, e.getMessage());
         }
     }
 }
