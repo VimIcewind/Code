9033328c99e9d91b0e845f35496e78ecb8b0a215
VimIcewind <VimIcewind@gmail.com>
2017-06-23 17:37:39 +0800

    Just format code

--- c/thread/mthread.c
+++ c/thread/mthread.c
@@ -5,43 +5,43 @@
 #include <unistd.h>
 #include <sys/types.h>
 
-void* threadFunc(void* arg) {
-        printf("Before malloc in thread 1n");
-        getchar();
-        char* addr = (char*) malloc(1000);
-        printf("After malloc and before free in thread 1n");
-        getchar();
-        free(addr);
-        printf("After free in thread 1n");
-        getchar();
+void *threadFunc(void *arg)
+{
+	printf("Before malloc in thread 1n");
+	getchar();
+	char *addr = (char *)malloc(1000);
+	printf("After malloc and before free in thread 1n");
+	getchar();
+	free(addr);
+	printf("After free in thread 1n");
+	getchar();
 }
 
-int main() {
-        pthread_t t1;
-        void* s;
-        int ret;
-        char* addr;
+int main()
+{
+	pthread_t t1;
+	void *s;
+	int ret;
+	char *addr;
 
-        printf("Welcome to per thread arena example::%dn",getpid());
-        printf("Before malloc in main threadn");
-        getchar();
-        addr = (char*) malloc(1000);
-        printf("After malloc and before free in main threadn");
-        getchar();
-        free(addr);
-        printf("After free in main threadn");
-        getchar();
-        ret = pthread_create(&t1, NULL, threadFunc, NULL);
-        if(ret)
-        {
-                printf("Thread creation errorn");
-                return -1;
-        }
-        ret = pthread_join(t1, &s);
-        if(ret)
-        {
-                printf("Thread join errorn");
-                return -1;
-        }
-        return 0;
+	printf("Welcome to per thread arena example::%dn", getpid());
+	printf("Before malloc in main threadn");
+	getchar();
+	addr = (char *)malloc(1000);
+	printf("After malloc and before free in main threadn");
+	getchar();
+	free(addr);
+	printf("After free in main threadn");
+	getchar();
+	ret = pthread_create(&t1, NULL, threadFunc, NULL);
+	if (ret) {
+		printf("Thread creation errorn");
+		return -1;
+	}
+	ret = pthread_join(t1, &s);
+	if (ret) {
+		printf("Thread join errorn");
+		return -1;
+	}
+	return 0;
 }
