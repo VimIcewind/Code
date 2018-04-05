8f2d558082a39fb2ff1d4a3282b9619b743795fa
VimIcewind <VimIcewind@gmail.com>
2018-04-05 22:06:25 +0800

    plus1 and minus1

---
+++ lisp/plus1.scm
@@ -0,0 +1,9 @@
+; plus 1
+(define plus1
+  (lambda (num)
+    (+ num 1)))
+
+; minus 1
+(define minus1
+  (lambda (num)
+    (- num 1)))
---
+++ lisp/plus11.scm
@@ -0,0 +1,7 @@
+; plus 1
+(define (plus1 num)
+  (+ num 1))
+
+; minus 1
+(define (minus1 num)
+  (- num 1))
