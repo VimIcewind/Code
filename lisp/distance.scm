dd1426c11623fdf6827a34b27aef6b66e5a96a62
VimIcewind <VimIcewind@gmail.com>
2018-04-05 22:06:41 +0800

    distance

---
+++ lisp/distance.scm
@@ -0,0 +1,20 @@
+; definition of pi
+(define pi (* 4 (atan 1.0)))
+
+; degree -> radian
+(define (radian deg)
+  (* deg (/ pi 180.0)))
+
+; free fall time
+(define (ff-time vy)
+  (/ (* 2.0 vy) 9.8))
+
+; horizontal distance
+(define (dx vx t)
+  (* vx t))
+
+; distance
+(define (distance v ang)
+  (dx
+   (* v (cos (radian ang)))		; vx
+   (ff-time (* v (sin (radian ang))))))	; t
