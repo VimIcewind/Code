98bd2cb4bfddcd9278d0ca5234f56057c1455619
VimIcewind <VimIcewind@gmail.com>
2019-03-18 17:32:49 +0800

    Node.js server demo Hi1

---
+++ node/Hi1.js
@@ -0,0 +1,10 @@
+var http = require("http");
+
+function onRequest(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}
+
+http.createServer(onRequest).listen(8888);
+console.log('Server runing at http://127.0.0.1:8888');
