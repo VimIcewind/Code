4884700ce0b108e5b2904dccac1e0974601a4594
VimIcewind <VimIcewind@gmail.com>
2019-04-04 22:27:10 +0800

    hello

--- c/csapp/intro/decode1.c
+++
@@ -1,9 +0,0 @@
-void decode1(int *xp, int *yp, int *zp)
-{
-	int tmp;
-
-	tmp = *xp;
-	*xp = *zp;
-	*zp = *yp;
-	*yp = tmp;
-}
--- c/csapp/intro/simple.c
+++
@@ -1,6 +0,0 @@
-int simple(int *xp, int y)
-{
-	int t = *xp + y;
-	*xp = t;
-	return t;
-}
