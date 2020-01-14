0e256df34f2d45a3667fed190339994b5d194ac1
VimIcewind <VimIcewind@gmail.com>
2020-01-14 13:58:33 +0800

    fork

---
+++ c/process/fork.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	pid_t pid;
+	char *message;
+	int n;
+	pid = fork();
+
+	if (pid < 0) {
+		perror("fork failed");
+	}
+	if (pid == 0) {
+		message = "This is the child\n";
+		n = 6;
+	} else {
+		message = "This is the parent\n";
+		n = 3;
+	}
+
+	for (; n > 0; n--) {
+		printf(message);
+		sleep(1);
+	}
+	return 0;
+}
