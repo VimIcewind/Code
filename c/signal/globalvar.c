d1fac45b17a6b94889fdf479b80e7818a6ac7244
VimIcewind <VimIcewind@gmail.com>
2020-07-02 20:26:59 +0800

    volatile

---
+++ c/signal/globalvar.c
@@ -0,0 +1,7 @@
+long long a;
+
+int main(int argc, const char *argv[])
+{
+	a = 5;
+	return 0;
+}
---
+++ c/signal/sig_atomic_t.c
@@ -0,0 +1,11 @@
+#include <signal.h>
+
+sig_atomic_t a = 0;
+
+int main(int argc, const char *argv[])
+{
+	/* register a sighandler */
+	while (!a); /* wait unitl a changes in sighandler */
+	/* do something after signal arrives */
+	return 0;
+}
