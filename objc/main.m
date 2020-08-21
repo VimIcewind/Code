a3cf3c252296c7ee3bbdb180d831c60216c7a8e7
VimIcewind <VimIcewind@gmail.com>
2020-08-21 22:20:05 +0800

    objc message

--- objc/main.m
+++ objc/main.m
@@ -5,6 +5,10 @@ int main(int argc, const char *argv[])
 	@autoreleasepool {
 		NSDate *now = [NSDate date];
 		NSLog(@"This NSDate object lives at %p", now);
+		NSLog(@"The date is %@", now);
+
+		double seconds = [now timeIntervalSince1970];
+		NSLog(@"It has been %f seconds since the start of 1970.", seconds);
 	}
 	return 0;
 }
