da5ec46db225b406e70cc7d80de53f286dc344a4
VimIcewind <VimIcewind@gmail.com>
2020-11-23 15:27:44 +0800

    open close read write

---
+++ c/syscall/blockread.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(void)
+{
+	char buf[10];
+	int n;
+
+	n = read(STDIN_FILENO, buf, 10);
+	if (n < 0) {
+		perror("read STDIN_FILENO");
+		exit(1);
+	}
+	write(STDOUT_FILENO, buf, n);
+
+	return 0;
+}
---
+++ c/syscall/nonblockread.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MSG_TRY "try again\n"
+
+int main(void)
+{
+	char buf[10];
+	int fd, n;
+
+	fd = open("/dev/tty", O_RDONLY|O_NONBLOCK);
+	if (fd < 0) {
+		perror("open /dev/tty");
+		exit(1);
+	}
+tryagain:
+	n = read(fd, buf, 10);
+	if (n < 0) {
+		if (errno == EAGAIN) {
+			sleep(1);
+			write(STDOUT_FILENO, MSG_TRY, strlen(MSG_TRY));
+			goto tryagain;
+		}
+		perror("read /dev/tty");
+		exit(1);
+	}
+	write(STDOUT_FILENO, buf, n);
+	close(fd);
+
+	return 0;
+}
