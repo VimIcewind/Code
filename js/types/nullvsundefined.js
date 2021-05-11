3c45ecb4669b2fa18b2921b83f7159a508fe1122
VimIcewind <VimIcewind@gmail.com>
2021-05-11 16:12:39 +0800

    undefine null false 0 NaN '' "" is false when translate to boolean

--- js/types/nullvsundefined.js
+++ js/types/nullvsundefined.js
@@ -31,6 +31,19 @@ if (!"") {
 }
 // "" is false
 
+console.log(Boolean(undefined))
+// false
+console.log(Boolean(null))
+// false
+console.log(Boolean(0))
+// false
+console.log(Boolean(false))
+// false
+console.log(Boolean(''))
+// false
+console.log(Boolean(""))
+// false
+
 
 console.log(undefined == null);
 // true
