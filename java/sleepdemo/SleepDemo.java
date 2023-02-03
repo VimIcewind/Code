12b2714a43fe7937e8df894a13a55ee02a6cf592
VimIcewind <VimIcewind@gmail.com>
2023-02-03 22:28:41 +0800

    java sleep

---
+++ java/sleepdemo/SleepDemo.java
@@ -0,0 +1,7 @@
+public class SleepDemo {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("sleep(6000) start:");
+        Thread.sleep(60000);
+        System.out.println("sleep(6000) done.");
+    }
+}
