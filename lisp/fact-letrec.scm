ad6a26fb51407515d18c587538e66060a7efcb53
VimIcewind <VimIcewind@gmail.com>
2024-10-31 16:35:38 +0800

    Scheme letrec

--- lisp/fact-letrec.scm
+++ lisp/fact-letrec.scm
@@ -1,7 +1,9 @@
 (define (fact-letrec n)
   (letrec ((iter (lambda (n1 p)
-		   (if (= n1 1)
-		       p
-		       (let ((m (- n1 1)))
-			 (iter m (* p m)))))))
+                   (if (= n1 1)
+                     p
+                     (let ((m (- n1 1)))
+                       (iter m (* p m)))))))
     (iter n n)))
+
+(fact-letrec 5)
