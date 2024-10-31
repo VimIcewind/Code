085166604e7443a78bdec07b9a0fafa27db0f8c4
VimIcewind <VimIcewind@gmail.com>
2024-10-31 16:23:56 +0800

    Scheme named let

--- lisp/fact-let.scm
+++ lisp/fact-let.scm
@@ -1,6 +1,8 @@
 (define (fact-let n)
   (let loop((n1 n) (p n))
     (if (= n1 1)
-	p
-	(let ((m (- n1 1)))
-	  (loop m (* p m))))))
+      p
+      (let ((m (- n1 1)))
+        (loop m (* p m))))))
+
+(fact-let 5)
