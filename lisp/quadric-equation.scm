dba09e5b2a6435ca830d32578dffa2c4eecc7697
VimIcewind <VimIcewind@gmail.com>
2024-10-31 15:12:25 +0800

    Scheme quadric-equation

--- lisp/quadric-equation.scm
+++ lisp/quadric-equation.scm
@@ -2,13 +2,16 @@
 
 (define (quadric-equation a b c)
   (if (zero? a)
-      'error
-      (let ((d (- (* b b) (* 4 a c))))
-	(if (negative? d)
-	    '()
-	    (let ((e (/ b a -2)))
-	      (if (zero? d)
-		  (list e)
-		  (let ((f (/ (sqrt d) a 2)))
-		    (list (+ e f) (- e f)))))))))
+    'error
+    (let ((d (- (* b b) (* 4 a c))))
+      (if (negative? d)
+        '()
+        (let ((e (/ b a -2)))
+          (if (zero? d)
+            (list e)
+            (let ((f (/ (sqrt d) a 2)))
+              (list (+ e f) (- e f)))))))))
 
+; solution of 3x^2+5x+2=0
+; (-2/3 -1)
+(quadric-equation 3 5 2)
