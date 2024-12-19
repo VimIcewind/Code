97d701b892dd242214739b6ffc7eedc552090755
VimIcewind <VimIcewind@gmail.com>
2024-12-19 15:55:09 +0800

    Elisp lambda funcall

--- lisp/funcall.el
+++ lisp/funcall.el
@@ -4,3 +4,18 @@
 
 (setq b (lambda (x) (+ x 2)))
 (funcall b 2)
+
+
+(setq foo (lambda (name)
+            (message "Hello, %s!" name)))
+;; error
+(foo "Tom")
+
+;; Hello, Tom!
+(funcall foo "Tom")
+
+;; Hello, Tom!
+((lambda (name)
+   (message "Hello, %s!" name))
+ "Tom")
+
