8f1217659951e131a0f99dbee843aaa7f134371d
VimIcewind <VimIcewind@gmail.com>
2025-01-03 16:03:15 +0800

    Cuda demo

---
+++ cuda/test.cu
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	int device_id = 0;
+	cudaDeviceProp prop;
+
+	cudaGetDeviceProperties(&prop, device_id);
+
+	printf("Device name: %s\n", prop.name);
+	printf("Compute capability: %d.%d\n", prop.major, prop,minor);
+
+	return 0;
+}
